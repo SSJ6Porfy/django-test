8aa79673c6fc457b7a834587276423ad276e9981
Porfirio Matias <SSJ6Porfy@hotmail.com>
2017-10-08 18:52:58 -0700

    edited settings

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -97,9 +97,9 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
@@ -141,6 +141,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(os.path.join(BASE_DIR, 'frontend'), 'build', 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(os.path.join(BASE_DIR, 'frontend'), 'build', 'static'),
+# )
